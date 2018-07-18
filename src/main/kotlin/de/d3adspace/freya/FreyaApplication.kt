e3cf1557e1e46630c1898174fc6cac476d5ec749
Felix Klauke <info@felix-klauke.de>
2018-07-18 11:01:14 +0200

    Add comments for the main application.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- src/main/kotlin/de/d3adspace/freya/FreyaApplication.kt
+++ src/main/kotlin/de/d3adspace/freya/FreyaApplication.kt
@@ -4,10 +4,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+/**
+ * The central application featuring the eureka server used for service discovery.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 class FreyaApplication
 
+/**
+ * The JVM entry method. Will run the SpringBootApplication.
+ */
 fun main(args: Array<String>) {
+
     runApplication<FreyaApplication>(*args)
 }
